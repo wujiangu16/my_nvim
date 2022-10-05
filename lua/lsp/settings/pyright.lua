301f42aa590747f27252294b89d00111fa108b05
wujiangu16 <wujiangu16@gmail.com>
2022-10-06 01:01:04 +0800

    test

---
+++ lua/lsp/settings/pyright.lua
@@ -0,0 +1,10 @@
+return {
+    settings = {
+        python = {
+            pythonPath = "/opt/anaconda3/envs/pytorch/bin/python",
+            analysis = {
+                typeCheckingMode = "off"
+            }
+        }
+    },
+}
